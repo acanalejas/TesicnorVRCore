5158ca9a1455a29d5e0c16275d680471d09ef331
acanalejas <acanalejas@tesicnor.com>
2022-10-21 08:55:15 +0200

    Update FireObject.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
@@ -498,7 +498,7 @@ namespace TesicFire
             distances = distances.OrderByDescending(x => x).ToList();
             Debug.Log(meshData_current.vertex.Count);
 
-            for(int k = 0; k < distances.Count; k++)
+            for(int k = 0; k < numVertex; k++)
             {
                 meshData_current.vertex.Add(meshData_original.vertex[distances[k].index]);
             }
