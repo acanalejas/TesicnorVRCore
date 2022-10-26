ba6f407f61cb85706cdf41f9ad66dcd754cc38cd
acanalejas <acanalejas@tesicnor.com>
2022-10-26 16:38:38 +0200

    Update FireObject.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
@@ -670,7 +670,7 @@ namespace TesicFire
         public void OnDrawGizmos()
         {
             Vector3 size = fire_MR.localBounds.size + PropOffset; Vector3 scale = transform.lossyScale;
-            Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Matrix4x4 rotationMatrix = transform.localToWorldMatrix;
             Gizmos.matrix = rotationMatrix;
             Gizmos.DrawWireCube(fire_MR.bounds.center, transform.InverseTransformDirection(new Vector3(size.x * scale.x, size.y * scale.y, size.z * scale.z)));
         }
