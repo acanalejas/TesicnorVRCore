99f09ae8845d82e68f3ff661d565d6fb64efca67
acanalejas <acanalejas@tesicnor.com>
2022-10-26 11:37:18 +0200

    Update FireObject.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Fire/FireObject.cs
@@ -669,7 +669,8 @@ namespace TesicFire
 
         public void OnDrawGizmos()
         {
-            Gizmos.DrawWireCube(fire_MR.bounds.center, fire_MR.localBounds.size + PropOffset);
+            Vector3 size = fire_MR.localBounds.size + PropOffset; Vector3 scale = transform.lossyScale;
+            Gizmos.DrawWireCube(fire_MR.bounds.center, new Vector3(size.x * scale.x, size.y*scale.y, size.z * scale.z);
         }
 
         public void OnTriggerStay(Collider other)
