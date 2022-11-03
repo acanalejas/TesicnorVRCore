d29215b31b79f850a2153a59a3c3dfb79c2758db
acanalejas <acanalejas@tesicnor.com>
2022-11-03 16:17:38 +0100

    DeadScene

---
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Utilities/DeadScene.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class DeadScene : MonoBehaviour
+{
+    #region PARAMETERS
+    private static int menuIndex;
+    private static int lastSceneIndex;
+
+    static string deadScene_name = "DeadScene";
+    static string deadScene_results = "";
+    static string deadScene_retry = "RETRY";
+    static string deadScene_goToMenu = "GO TO MENU";
+
+    public TextMeshProUGUI results;
+    public Button MenuButton;
+    public Button RetryButton;
+    #endregion
+
+    #region FUNCTIONS
+    public static void GoToDeadScene(int _menuIndex, int _lastSceneIndex, string _results, string retryTraduction = "RETRY", string goToMenuTraduction = "GO TO MENU")
+    {
+        menuIndex = _menuIndex; lastSceneIndex = _lastSceneIndex; deadScene_results = _results;
+        deadScene_retry = retryTraduction; deadScene_goToMenu = goToMenuTraduction;
+
+        SceneManager.LoadScene(deadScene_name);
+    }
+
+    private void Start()
+    {
+        results.text = deadScene_results;
+
+        MenuButton.onClick.AddListener(GoToMenu);
+        RetryButton.onClick.AddListener(Retry);
+
+        MenuButton.GetComponentInChildren<TextMeshProUGUI>().text = deadScene_goToMenu;
+        RetryButton.GetComponentInChildren<TextMeshProUGUI>().text = deadScene_retry;
+    }
+
+    private void GoToMenu()
+    {
+        SceneManager.LoadScene(menuIndex);
+    }
+    private void Retry()
+    {
+        SceneManager.LoadScene(lastSceneIndex);
+    }
+    #endregion
+}
