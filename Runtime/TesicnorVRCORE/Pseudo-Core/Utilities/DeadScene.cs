838cdb3c8c1e2c217b8eaba50a0cb0cd3a3d699a
acanalejas <acanalejas@tesicnor.com>
2022-11-08 11:37:58 +0100

    Update DeadScene.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Utilities/DeadScene.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Utilities/DeadScene.cs
@@ -8,6 +8,7 @@ using TMPro;
 public class DeadScene : MonoBehaviour
 {
     #region PARAMETERS
+    public static DeadScene Instance;
     private static int menuIndex;
     private static int lastSceneIndex;
 
@@ -19,9 +20,22 @@ public class DeadScene : MonoBehaviour
     public TextMeshProUGUI results;
     public Button MenuButton;
     public Button RetryButton;
+
+    [Header("Para el player")]
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+    [HideInInspector] public List<Component> ForButtons;
+
+    [HideInInspector] public List<Component> ForRightHand_Components;
+    [HideInInspector] public List<GameObject> ForRightHand_GO;
+
+    [HideInInspector] public List<Component> ForLeftHand_Components;
+    [HideInInspector] public List<GameObject> ForLeftHand_GO;
     #endregion
 
     #region FUNCTIONS
+    #region Static functions
     public static void GoToDeadScene(int _menuIndex, int _lastSceneIndex, MonoBehaviour _class, string _function , string retryTraduction = "RETRY", string goToMenuTraduction = "GO TO MENU")
     {
         menuIndex = _menuIndex; lastSceneIndex = _lastSceneIndex;  _class.Invoke(_function, 0.0f);
@@ -29,15 +43,112 @@ public class DeadScene : MonoBehaviour
 
         SceneManager.LoadScene(deadScene_name);
     }
-
     public static void SetDeadText(string input)
     {
         deadScene_results = input;
     }
 
+    public static void AddComponentsToButtons(List<Component> toAdd)
+    {
+        if (Instance == null) return;
+
+        Instance.ForButtons.Clear();
+        Instance.ForButtons.AddRange(toAdd);
+    }
+
+    public static void AddComponentsToRightHand(List<Component> toAdd)
+    {
+        if (Instance == null) return;
+
+        Instance.ForRightHand_Components.Clear();
+        Instance.ForRightHand_Components.AddRange(toAdd);
+    }
+
+    public static void AddComponentsToRightHand(List<GameObject> toAdd)
+    {
+        if (Instance == null) return;
+
+        Instance.ForRightHand_GO.Clear();
+        Instance.ForRightHand_GO.AddRange(toAdd);
+    }
+
+    public static void AddComponentsToLeftHand(List<Component> toAdd)
+    {
+        if (Instance == null) return;
+
+        Instance.ForLeftHand_Components.Clear();
+        Instance.ForLeftHand_Components.AddRange(toAdd);
+    }
+    public static void AddComponentsToLeftHand(List<GameObject> toAdd)
+    {
+        if (Instance == null) return;
+
+        Instance.ForLeftHand_GO.Clear();
+        Instance.ForLeftHand_GO.AddRange(toAdd);
+    }
+    #endregion
+
+    private void SetComponentsForButtons()
+    {
+        if (ForButtons.Count <= 0) return;
+        
+        foreach(Component comp in ForButtons)
+        {
+            Component menuComp = MenuButton.gameObject.AddComponent(comp.GetType());
+            menuComp = comp;
+            
+            Component retryComp = RetryButton.gameObject.AddComponent(comp.GetType());
+            retryComp = comp;
+        }
+    }
+
+    private void SetComponentsHands()
+    {
+        if(ForRightHand_Components.Count > 0)
+        {
+            foreach(Component comp in ForRightHand_Components)
+            {
+                Component _comp = rightHand.gameObject.AddComponent(comp.GetType());
+                _comp = comp;
+            }
+        }
+        if(ForLeftHand_Components.Count > 0)
+        {
+            foreach(Component comp in ForLeftHand_Components)
+            {
+                Component _comp = leftHand.gameObject.AddComponent(comp.GetType());
+                _comp = comp;
+            }
+        }
+    }
+
+    private void SetGameObjectsForHands()
+    {
+        if(ForRightHand_GO.Count > 0)
+        {
+            foreach(GameObject go in ForRightHand_GO)
+            {
+                GameObject newGo = GameObject.Instantiate(go, rightHand.transform);
+            }
+        }
+
+        if(ForLeftHand_GO.Count > 0)
+        {
+            foreach(GameObject go in ForLeftHand_GO)
+            {
+                GameObject newGo = GameObject.Instantiate(go, leftHand.transform);
+            }
+        }
+    }
+
     private void Start()
     {
+        if (Instance == null) Instance = this; else Destroy(this);
         results.text = deadScene_results;
+
+        SetComponentsForButtons();
+        SetComponentsHands();
+        SetGameObjectsForHands();
 
         MenuButton.onClick.AddListener(GoToMenu);
         RetryButton.onClick.AddListener(Retry);
