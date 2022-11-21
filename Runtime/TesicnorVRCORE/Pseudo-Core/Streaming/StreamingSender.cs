8e5a7bd81354b0fafe09af19a3bce443ddc2ba5f
acanalejas <acanalejas@tesicnor.com>
2022-11-21 14:47:19 +0100

    Update StreamingSender.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/StreamingSender.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/StreamingSender.cs
@@ -29,7 +29,7 @@ public class StreamingSender : MonoBehaviour
     }
     private void SetTextureForCamera()
     {
-        Camera mainCamera = Camera.main;
+        Camera mainCamera = Camera.current;
 
         GameObject capturadora_go = new GameObject("capturadora", typeof(Camera));
         capturadora_go.transform.parent = mainCamera.gameObject.transform;
