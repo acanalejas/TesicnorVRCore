1939605ed6aba8daf1c954dcd7112039d423a69c
acanalejas <acanalejas@tesicnor.com>
2022-11-21 11:03:35 +0100

    Update StreamingSender.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/StreamingSender.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/StreamingSender.cs
@@ -63,7 +63,7 @@ public class StreamingSender : MonoBehaviour
         byte[] jpg = GetTextureTraduction();
         //File.WriteAllBytes(path, jpg);
         await HttpClient_Custom.SendData(jpg);
-        System.Threading.Thread.Sleep(50);
+        System.Threading.Thread.Sleep(100);
         alreadySended = true;
 
         return;
