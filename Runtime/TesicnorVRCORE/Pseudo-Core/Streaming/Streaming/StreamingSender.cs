1a4c9f686502f98ca8d9314935be4978b257ede3
acanalejas <acanalejas@tesicnor.com>
2022-11-30 10:10:47 +0100

    Update StreamingSender.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/Streaming/StreamingSender.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Streaming/Streaming/StreamingSender.cs
@@ -58,6 +58,7 @@ public class StreamingSender : MonoBehaviour
     Rect rect = new Rect(0, 0, 640, 480);
     private async void GetTextureTraduction()
     {
+        RenderTexture.active = captured;
         capturadora.Render();
         parse.ReadPixels(rect, 0, 0, false);
         _data = parse.GetRawTextureData();
