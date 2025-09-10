ddc802dc55c3f9c4390dd16620b1a8bcf58d2a82
AlbertoCanalejasRolldBox <135691614+AlbertoCanalejasRolldBox@users.noreply.github.com>
2025-09-10 15:09:02 +0200

    Update PlayerGravity.cs

--- Runtime/TesicnorVRCORE/Pseudo-Core/Utilities/PlayerGravity.cs
+++ Runtime/TesicnorVRCORE/Pseudo-Core/Utilities/PlayerGravity.cs
@@ -123,14 +123,14 @@ public class PlayerGravity : MonoBehaviour
                 }
                 else if(timer != 0)
                 {
-                    if(timer >= 0.125f) OnFallEnd.Invoke(IsPlayerAnchored());
+                    if(timer >= 0.03f) OnFallEnd.Invoke(IsPlayerAnchored());
                     timer = 0;
                 }
                 if (HMD_pd && this.BodyColl) this.BodyColl.height = HMD_pd.positionInput.action.ReadValue<Vector3>().y + 0.2f;
             }
              else if(timer != 0) {
 
-                if (timer >= 0.05f) OnFallEnd.Invoke(IsPlayerAnchored());
+                if (timer >= 0.03f) OnFallEnd.Invoke(IsPlayerAnchored());
                 timer = 0;
             }
             //AdjustHeight();
